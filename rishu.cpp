b099e917433cb0892967c100b6010368c7b325f8
RISHABH KUMAR <chaubeyr1234@gmail.com>
2024-06-04 12:43:40 +0530

    first commit

---
+++ rishu.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hello";
+    
+    
+}
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/rishabhkumarchaubey/C-DSA.git
+git push -u origin main
