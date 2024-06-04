33aed18cace43cc03a3395fe824461334abf9bc6
RISHABH KUMAR <chaubeyr1234@gmail.com>
2024-06-04 12:44:32 +0530

    update code

--- rishu.cpp
+++ rishu.cpp
@@ -6,10 +6,3 @@ int main()
     
     
 }
-
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/rishabhkumarchaubey/C-DSA.git
-git push -u origin main
